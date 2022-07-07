944441b4a7e67279f9ccb51261cf42e4871df20c
Hirata-Kodai <hiratako@ist.hokudai.ac.jp>
2022-07-07 15:57:56 +0900

    DB の接続を見越し、処理を関数化。 コード中の memos はデータベースから取ってくるイメージ。

--- docs/main.js
+++ docs/main.js
@@ -1,21 +1,59 @@
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
+const addTaskForm = document.querySelector('#add_task');
+const memos_ul = document.querySelector("#memos");
+
+let memos = [
+	{
+		id: 0,
+		value: "aaa"
+	}
+];  // List[structure]
+
+addTaskForm.addEventListener('submit', (e) => {
+	e.preventDefault();
     const input = document.querySelector('#add_new_memo');
     const input_value = input.value
 	if (input_value !== '') {
-		console.log(input_value);
-		input.value = '';
-		const newLi = document.createElement('li');
-		newLi.innerText = input_value+' ';
-		const newButton = document.createElement('button');
-		newButton.innerText = 'delete';
-		newLi.appendChild(newButton);
-		newButton.addEventListener('click', () => {
-			document.querySelector('ul').removeChild(newLi);
-		});
-		document.querySelector('ul').appendChild(newLi);
+		appendMemo(input_value);
+		addTaskForm.reset();
+		appendChildLiTag(input_value);
 	}
 	else {
 		alert('please input');
 	}
 });
+
+// 後々データベースから取ってくる処理
+let fetchMemos = () => {
+	return memos;
+};
+
+let initialRender = () => {
+	const init_memos = fetchMemos();
+	for (let i=0; i < memos.length; i++) {
+		appendChildLiTag(init_memos[i].value + ' ');
+	}
+}
+
+// memos に structure を追加する処理
+let appendMemo = (input_value) => {
+	memos.push(
+		{
+			id: memos.length,
+			value: input_value
+		}
+	);
+}
+
+// submit された input_value を innerText を持つ li を ul の子要素のラストに追加する
+let appendChildLiTag = (input_value) => {
+	const newLi = document.createElement('li');
+	newLi.innerText = input_value + ' ';
+	const newButton = document.createElement('button');
+	newButton.innerText = 'delete';
+	newLi.appendChild(newButton);
+	newButton.addEventListener('click', () => {
+		memos_ul.removeChild(newLi);
+	});
+	memos_ul.appendChild(newLi);
+}
+initialRender();
